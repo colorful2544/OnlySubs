0efe0ade70e39884dcfaa77bce7c03b272ace1e4
Bike Chanokchon <mint.colorful@gmail.com>
2021-10-15 19:22:36 +0700

    Add UserService

---
+++ Services/UserService/IUserService.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using OnlySubs.Models.db;
+using OnlySubs.ViewModels.Requests;
+
+namespace OnlySubs.Services.UserService
+{
+    public interface IUserService
+    {
+        Task CreateAsync(UserRegisterRequest userRegisterRequest);
+        Task<User> FindByUsernameAsync(string username);
+        Task<User> FindByUserIdAsync(string userId);
+        Task UpdateAsync(UserUpdateRequest userUpdateRequest);
+        Task RemoveAsync(string userId);
+        Task<int> FindFollowerCount(string userId);
+        Task<int> FindFollowingCount(string userId);   
+    }
+}
